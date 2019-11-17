a6b035d271898d48a20ff459d06e8b8d44083530
Dr. XyClo <sdcarmody@gmail.com>
2019-11-17 10:25:26 +0000

    Automatically backed up by Learn

--- spec/metadancing_spec.rb
+++ spec/metadancing_spec.rb
@@ -2,6 +2,7 @@ describe 'MetaDancing' do
   let(:dummy_class) {Class.new {extend MetaDancing}}
 
   it 'has a metadata method' do 
+    binding.pry
     expect(dummy_class.metadata).to eq("This class produces objects that love to dance.")
   end
 end
